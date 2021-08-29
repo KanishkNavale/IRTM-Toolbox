6d6b787a7ddf726d5b26da0c4e63158e70ad019e
Kanishk Navale <navalekanishk@gmail.com>
2021-08-29 11:23:29 +0200

    package init.

---
+++ irtm/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='irtm',
+      version='0.0.1',
+      description='This package holds pivotal functions for IRTM processing',
+      url='git@github.com:KanishkNavale/IRTM-Toolbox.git',
+      author='Kanishk Navale',
+      author_email='navalekanishk@gmail.com',
+      license='MIT',
+      packages=['irtm'],
+      zip_safe=False)
