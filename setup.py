40fd5cdd8cf0afaf4c56d7ad0e0b785dc1ee3b4b
Kanishk Navale <navalekanishk@gmail.com>
2021-08-29 17:49:39 +0200

    Build Fixes 2

--- setup.py
+++ setup.py
@@ -1,23 +1,40 @@
 import setuptools
 
+
 # Readme.md Handler
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setuptools.setup(name='irtm',
+setuptools.setup(
+                 # Package Name
+                 name='irtm',
+
+                 # Version ID
                  version='0.0.1',
+
+                 # Package Descriptions
                  description='A toolbox for Information Retreival & Text Mining.',
                  long_description=long_description,
                  long_description_content_type="text/markdown",
                  url='https://github.com/KanishkNavale/IRTM-Toolbox.git',
+
+                 # Developer Details
                  author='Kanishk Navale',
                  author_email='navalekanishk@gmail.com',
+
+                 # License
                  license='MIT',
+
+                 # Package Build Source Pointer
                  package_dir={"": "src"},
                  packages=setuptools.find_packages(where="src"),
+
+                 # Setup Prequisites
                  install_requires=[
                                     'nltk==3.5'
                                   ],
+
+                 # WrapUP
                  include_package_data=True,
                  zip_safe=False,
                  )
