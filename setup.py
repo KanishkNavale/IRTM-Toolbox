664b47af43e7d604f7046a49ee9fe1ed4c2c8dd4
Kanishk Navale <navalekanishk@gmail.com>
2021-08-29 18:57:45 +0200

    Adding Support for Windows

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setuptools.setup(
                  name='irtm',
 
                  # Version ID
-                 version='0.0.1',
+                 version='0.0.2',
 
                  # Package Descriptions
                  description='A toolbox for Information Retreival & Text Mining.',
